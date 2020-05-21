21a002d43fdcfcd09196ac3268a83f43eb4101fc
iamOlivier <olivieryannick5@gmail.com>
2020-05-21 12:37:04 -0600

    Changed menu bar to sidebar

--- main.js
+++ main.js
@@ -7,15 +7,15 @@ let openMenuBar = function() {
   let menuProjLinks = document.querySelector(".open-menubar section a");
 
   if (menuStatus === false) {
-    menuBar.style.height = "100px";
+    menuBar.style.width = "300px";
     menuBarH2.style.visibility = "visible";
     menuProjLinks.style.visibility = "visible";
     menuStatus = true;
 
   } else {
+    menuBar.style.width = "0px";
     menuBarH2.style.visibility = "hidden";
     menuProjLinks.style.visibility = "hidden";
-    menuBar.style.height = "0px";
     menuStatus = false;
   }
 
